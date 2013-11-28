e417b5756d3c009a920b1eb3d54189bd7af7f005
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2013-11-28 17:53:24 +0100

    Ditch JAXB in favor of XmlBeans

--- src/main/java/org/qucosa/fcrepo/migration/Main.java
+++ src/main/java/org/qucosa/fcrepo/migration/Main.java
@@ -17,16 +17,17 @@
 
 package org.qucosa.fcrepo.migration;
 
+import fedora.fedoraSystemDef.foxml.DigitalObjectDocument;
+import fedora.fedoraSystemDef.foxml.DigitalObjectType;
+import fedora.fedoraSystemDef.foxml.ObjectPropertiesType;
+import fedora.fedoraSystemDef.foxml.PropertyType;
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.SystemConfiguration;
-import org.qucosa.fcrepo.foxml.DigitalObject;
+import org.apache.xmlbeans.XmlOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 
 import static java.lang.System.exit;
 
@@ -35,30 +36,37 @@ public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        QucosaProvider qucosaProvider = new QucosaProvider();
         try {
-            Configuration conf = getConfiguration();
-            qucosaProvider.configure(conf);
+            DigitalObjectDocument dof = DigitalObjectDocument.Factory.newInstance();
 
-            DigitalObject fdo = new DigitalObject();
-            printXml(fdo);
+            DigitalObjectDocument.DigitalObject dobj = dof.addNewDigitalObject();
+            dobj.setVERSION(DigitalObjectType.VERSION.X_1_1);
 
+            ObjectPropertiesType pt = dobj.addNewObjectProperties();
+
+            {
+                PropertyType p = pt.addNewProperty();
+                p.setNAME(PropertyType.NAME.INFO_FEDORA_FEDORA_SYSTEM_DEF_MODEL_STATE);
+                p.setVALUE("Active");
+            }
+            {
+                PropertyType p = pt.addNewProperty();
+                p.setNAME(PropertyType.NAME.INFO_FEDORA_FEDORA_SYSTEM_DEF_MODEL_LABEL);
+                p.setVALUE("Synthetic Demo Object");
+            }
+            {
+                PropertyType p = pt.addNewProperty();
+                p.setNAME(PropertyType.NAME.INFO_FEDORA_FEDORA_SYSTEM_DEF_MODEL_OWNER_ID);
+                p.setVALUE("SLUB");
+            }
+
+            dof.save(System.out, new XmlOptions().setSavePrettyPrint());
         } catch (Exception e) {
             log.error(e.getMessage());
             exit(1);
-        } finally {
-            qucosaProvider.release();
         }
     }
 
-    private static void printXml(DigitalObject fdo) throws JAXBException {
-        JAXBContext jaxbContext = JAXBContext.newInstance(DigitalObject.class);
-        Marshaller marshaller = jaxbContext.createMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        marshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, "info:fedora/fedora-system:def/foxml# " +
-                "http://www.fedora.info/definitions/1/0/foxml1-1.xsd");
-        marshaller.marshal(fdo, System.out);
-    }
 
     private static Configuration getConfiguration() throws ConfigurationException {
         return new SystemConfiguration();
